9f6f28f74d373df1121fa82ecf7d3465c0bbfae4
nevertheless0404 <nevertheless.hi@gmail.com>
2022-10-14 14:59:09 +0900

    review/detail

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,4 +7,5 @@ app_name = "reviews"
 urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
+    path("detail/<int:pk>", views.detail, name="detail"),
 ]
