8e62e2a61476dafe3ee41ff2ccd53193135354db
jupiter6676 <jupiter6676@naver.com>
2022-10-14 11:24:42 +0900

    login logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm    # 로그인
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import get_user_model
 
 # Create your views here.
@@ -23,3 +26,27 @@ def signup(request):
     }
 
     return render(request, "accounts/signup.html", context)
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect("accounts:index")
+
+    else:
+        form = AuthenticationForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request):
+    auth_logout(request)
+    
+    return redirect("accounts:index")
